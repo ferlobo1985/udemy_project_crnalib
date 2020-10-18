06773b62044ca397e4c87cb9b9f71750ca1f3adc
devdummy <dev.teach.user.dummy@gmail.com>
2020-10-17 22:54:39 -0300

    all code

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Icon, Header, Overlay }  from 'react-native-elements';
 
-export default function App() {
+const App = () => {
+  const [visible,setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Header
+        leftComponent={
+          <Icon
+            name="email"
+            color="#f50"
+            type="entypo"
+            onPress={()=> alert('open sidedrawer')}
+          />
+        }
+        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <Text>Hello</Text>
+      <Icon
+        name="rowing"
+      />
+
+        <Button title="open overlay" onPress={toggleOverlay}/>
+
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+        <Text>Hello from overlay</Text>
+      </Overlay>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
